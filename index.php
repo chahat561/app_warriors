c9995a977928f619b144d76926fb3c595b3710c3
Chahat Middha <chahatm@chetu.com>
2014-06-02 16:53:23 +0530

    changing the code in index file

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php echo "Testing";
-<<<<<<< HEAD
+
 //Adding the second set of changes in the file
 echo phpinfo();
 ?>
-=======
-Adding the second set of changes in the file
-?>
->>>>>>> chahatm-patch-1
+
+
+
+
